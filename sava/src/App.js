471e27d555bbcbd08ac931b9631d9c013662acdb
Abhijay Jain <Abhijay007j@gmail.com>
2021-10-10 15:03:59 +0530

    added boilerplate code

---
+++ sava/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>sava project</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
