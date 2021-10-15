82efe15ad361db3e9acff68d780ac32930342403
adarsh-technocrat <adarshkumarsingh865@gmail.com>
2021-10-15 19:32:31 +0530

    chore: add firebase config credientials

--- firebase.js
+++ firebase.js
@@ -3,13 +3,14 @@ import "firebase/auth";
 import "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
-  measurementId: "", // ...
+  apiKey: "AIzaSyCfKydelfz0r1CaMUY082Go-IHCsd4aW64",
+  authDomain: "sava-website-74608.firebaseapp.com",
+  databaseURL: "https://sava-website-74608-default-rtdb.firebaseio.com",
+  projectId: "sava-website-74608",
+  storageBucket: "sava-website-74608.appspot.com",
+  messagingSenderId: "681175771890",
+  appId: "1:681175771890:web:f12efc7a298e984ed97e6b",
+  measurementId: "G-C5MBQBGXS2",
 };
 
 firebase.initializeApp(firebaseConfig);
