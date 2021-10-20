6d30dcdcf7017bbaf2931b1926d4152cbda88f67
Abhijay Jain <Abhijay007j@gmail.com>
2021-10-20 13:58:04 +0530

    chore: added copanies banner

--- src/Pages/HomePage/index.js
+++ src/Pages/HomePage/index.js
@@ -1,12 +1,14 @@
 import { Box, makeStyles } from "@material-ui/core";
 import React from "react";
 import HeroSection from "../../Components/HomeComponents/heroSection";
+import CompanyWrapper from "../../Components/CompaniesDisplayComponents/CompanyWrapper";
 
 function HomePage() {
   const classes = useStyles();
   return (
     <Box className={classes.root}>
       <HeroSection />
+      <CompanyWrapper />
     </Box>
   );
 }
