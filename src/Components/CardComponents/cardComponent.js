ffafb287819115058d785e5a7a64e81685cc8869
adarsh-technocrat <adarshkumarsingh865@gmail.com>
2021-11-05 15:51:49 +0530

    fix:card layout for responsive

--- src/Components/CardComponents/cardComponent.js
+++ src/Components/CardComponents/cardComponent.js
@@ -1,6 +1,5 @@
 import React from "react";
 import Card from "@material-ui/core/Card";
-import CardMedia from "@material-ui/core/CardMedia";
 import CardContent from "@material-ui/core/CardContent";
 import Typography from "@material-ui/core/Typography";
 import { Box, makeStyles } from "@material-ui/core";
@@ -10,15 +9,15 @@ function CardComponent({ title, content, image, buttonTitle }) {
   const classes = useStyles();
   return (
     <Box className={classes.root}>
-      <Card className={classes.cardStyles}>
-        <CardMedia
-          component="img"
-          height="525"
-          image={image}
-          alt="green iguana"
-          background="#ffffff"
-        />
-        <CardContent className={classes.overlay}>
+      <Card
+        className={classes.cardStyles}
+        style={{
+          backgroundImage: `url(${image})`,
+          backgroundRepeat: "no - repeat",
+          backgroundSize: "cover",
+        }}
+      >
+        <CardContent>
           <Typography className={classes.titleText} color="textSecondary" gutterBottom>
             {title}
           </Typography>
@@ -44,75 +43,22 @@ function CardComponent({ title, content, image, buttonTitle }) {
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    background: "#ffffff",
+    padding: "40px",
   },
   cardStyles: {
-    position: "relative",
-    width: "425px",
-    [theme.breakpoints.down("lg")]: {},
-    [theme.breakpoints.down("md")]: {},
-    [theme.breakpoints.down("sm")]: {
-      width: "300px",
-    },
-  },
-  overlay: {
-    position: "absolute",
-    top: "90px",
-    left: "10px",
-    color: "black",
-    textAlign: "left",
-    backgroundColor: "transparent",
-  },
-  cardImage: {
-    background: "linear-gradient(0deg, rgba(34, 34, 34, 0.96) 1.34%, rgba(7, 7, 7, 0.11) 100%)",
+    height: "100%",
   },
   titleText: {
-    height: "44px",
-    fontWeight: "bold",
-    fontSize: "20px",
     color: "#FFFFFF",
-    padding: "15px 15px 0px 15px;",
-    [theme.breakpoints.down("lg")]: {},
-    [theme.breakpoints.down("md")]: {},
-    [theme.breakpoints.down("sm")]: {
-      height: "44px",
-      fontWeight: "bold",
-      fontSize: "15px",
-      color: "#FFFFFF",
-      padding: "0",
-    },
+    [theme.breakpoints.down("sm")]: {},
   },
   contentText: {
-    color: "#ffffff",
-    inlineSize: "350px",
-    overflowWrap: "break-word",
-    display: "flex",
-    padding: "0px 15px 15px 15px;",
-    fontStyle: "normal",
-    fontWeight: "normal",
-    fontSize: "16px",
-    lineHeight: " 22px",
-    letterSpacing: "0.2px",
-    [theme.breakpoints.down("lg")]: {},
-    [theme.breakpoints.down("md")]: {},
-    [theme.breakpoints.down("sm")]: {
-      color: "#ffffff",
-      inlineSize: "250px",
-      overflowWrap: "break-word",
-      display: "flex",
-      padding: "0",
-      fontStyle: "normal",
-      fontWeight: "normal",
-      fontSize: "16px",
-      lineHeight: " 22px",
-      letterSpacing: "0.2px",
-    },
+    [theme.breakpoints.down("sm")]: {},
   },
   Button: {
     flexDirection: " column",
     alignItems: "center",
     display: "flex",
-    marginTop: "40%",
   },
 }));
 
--- src/Components/CardSection/cardSection.js
+++ src/Components/CardSection/cardSection.js
@@ -15,24 +15,21 @@ function HeroSection() {
           pro-labour movement and believe that stipends must start at ₹10k/month.
         </Typography>
       </Box>
-      <Container>
-        <Box className={classes.wrapper}>
-          <Box className={classes.leftContainer}>
-            <CardComponent
-              image="assests/images/cardOne.gif"
-              title="What are we trying to do?"
-              content="Sava is an invite-only community of content creators looking to empower themselves by sharing what they’ve learnt overtime"
-              buttonTitle="Join our Community"
-            />
-          </Box>
-          <Box className={classes.rightContainer}>
-            <CardComponent
-              image="assests/images/cardTwo.gif"
-              title="What's the catch for Startups?"
-              content="Sava sources these brilliant Content Creators to startups who are looking for Creative Talent as interns."
-              buttonTitle="Learn More"
-            />
-          </Box>
+      <Container className={classes.container}>
+        <Box className={classes.wrapper} pt={10} pb={10}>
+          <CardComponent
+            image="assests/images/cardOne.gif"
+            title="What are we trying to do?"
+            content="Sava is an invite-only community of content creators looking to empower themselves by sharing what they’ve learnt overtime"
+            buttonTitle="Join our Community"
+          />
+
+          <CardComponent
+            image="assests/images/cardTwo.gif"
+            title="What's the catch for Startups?"
+            content="Sava sources these brilliant Content Creators to startups who are looking for Creative Talent as interns."
+            buttonTitle="Learn More"
+          />
         </Box>
       </Container>
     </Box>
@@ -45,39 +42,17 @@ const useStyles = makeStyles((theme) => ({
   },
   wrapper: {
     display: "flex",
-    height: "100vh",
-    width: "100%",
-    [theme.breakpoints.down("lg")]: {},
-    [theme.breakpoints.down("md")]: {},
+    height: "100%",
+    justifyContent: "center",
     [theme.breakpoints.down("sm")]: {
       flexDirection: "column",
-      height: "100%",
     },
   },
-  leftContainer: {
-    display: "flex",
-    flex: 4,
-    flexDirection: "column",
-    justifyContent: "center", // main Access alignment(vertical ) (y  axis)    &&   flexDirection: "row",   (x  axis)
-    alignItems: "center",
-    [theme.breakpoints.down("lg")]: {},
-    [theme.breakpoints.down("md")]: {},
-    [theme.breakpoints.down("sm")]: {
-      padding: "20px",
-    },
+
+  container: {
+    maxWidth: "80%",
   },
-  rightContainer: {
-    flex: 4,
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "center", // main Access alignment(vertical ) (y  axis)    &&   flexDirection: "row",   (x  axis)
-    alignItems: "center", // cross access alignment(Horizontal)(x  axis)  && flexDirection: "row",   (y  axis)
-    [theme.breakpoints.down("lg")]: {},
-    [theme.breakpoints.down("md")]: {},
-    [theme.breakpoints.down("sm")]: {
-      padding: "20px",
-    },
-  },
+
   title: {
     padding: "20px",
     paddingTop: "110px",
@@ -102,7 +77,6 @@ const useStyles = makeStyles((theme) => ({
   },
   subtitle2: {
     inlineSize: "700px",
-    overflowWrap: "break-word",
     color: "#808080",
     fontSize: "16px",
     lineHeight: "32px",
