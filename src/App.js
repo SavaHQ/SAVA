8886ab4c59e87f6568301ee6e0ab0c1987ddb3e7
Abhijay Jain <Abhijay007j@gmail.com>
2021-11-05 13:10:00 +0530

    chore: added scroll component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Router, useHistory } from "react-router-dom";
 import "./App.css";
+import ScrollComponent from "./Components/ScrollComponent";
 import Routes from "./Routes";
 
 function App() {
@@ -9,7 +10,7 @@ function App() {
     <div className="App">
       <Router history={history}>
         {/* <Auth> */}
-
+        <ScrollComponent />
         <Routes />
         {/* </Auth> */}
       </Router>
