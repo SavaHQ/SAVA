d3bd9572e3bb2dd2b1c4fb70306a6ee75b47b615
Aayush-hub <ayushgarg1951@gmail.com>
2021-11-22 14:08:44 +0530

    minor changes loader

--- src/Pages/OpeningsPage/index.js
+++ src/Pages/OpeningsPage/index.js
@@ -1,17 +1,18 @@
 import React from "react";
 import { Box, makeStyles } from "@material-ui/core";
 import Iframe from "react-iframe";
-
+import Loader from "react-loader-spinner";
 function OpeningsPage() {
   const classes = useStyles();
   return (
     <>
       <Box className={classes.iframe}>
+        <Loader type="Puff" color="#222222" height={600} width={100} timeout={1000} />
         <Iframe
           url="https://airtable.com/embed/shr8up3EecypZWnat?backgroundColor=teal&viewControls=on"
           width="100%"
           frameBorder="0"
-          height="533"
+          height="733"
           className="airtable-embed"
           styles={{ background: "transparent", border: "1px solid #ccc" }}
         ></Iframe>
